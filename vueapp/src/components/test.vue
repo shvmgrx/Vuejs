ec8619f7c52daa51665bde5e991aad180fa3d9e6
shvmgrx <s.goraksha@jacobs-university.de>
2017-08-22 04:28:57 +0530

    Few more code

--- vueapp/src/components/test.vue
+++ vueapp/src/components/test.vue
@@ -9,6 +9,19 @@
         <li v-for="item in items">{{item.title}}</li>
         </ul>
         <button v-on:click="greet(title)">Click me bruh</button>
+        <br>
+        <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+        <br>
+        <label>First name:</label>
+        <input type="text" v-model="user.firstName">
+        <br>
+        <label>Last name:</label>
+        <input type="text" v-model="user.lastName">
+        <br>
+        <h3>{{fullName}}</h3>
+        <br>
+        {{msg}}
+
 
         <!-- Alternative way to display
         <p v-text="user.firstName"></p>
@@ -19,6 +32,12 @@
 <script>
     export default {
         name: 'test',
+        props:{
+            msg:{
+                type:String,
+                default:'Foobar'
+            }
+        },
         data(){
             return{
                 title:'Coffee',
@@ -38,6 +57,17 @@
         methods:{
             greet: function(title){
                 alert('Moin Moin'+ " "+title);
+            },
+            pressKey: function(e){
+                console.log(e.target.value);
+            },
+            enterHit: function(){
+                console.log('You hit enter');
+            }
+        },
+        computed:{
+            fullName: function(){
+                return this.user.firstName+' '+this.user.lastName;
             }
         }
     }
